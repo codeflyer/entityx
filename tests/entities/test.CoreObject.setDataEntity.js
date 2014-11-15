85cd11c8da25656b4c14afb6097415ad0f31a623
Luca Pau <luca.pau82@gmail.com>
2014-11-16 00:25:46 +0100

    modifica setDataEntity in posso che possa gestire un array di modelli

--- tests/entities/test.CoreObject.setDataEntity.js
+++ tests/entities/test.CoreObject.setDataEntity.js
@@ -47,4 +47,46 @@ describe("Object: setDataEntity", function() {
             obj._setDataEntity('entity', 'TestModule/EntityInherit', 10);
         }).should.throw("Invalid identifier");
     });
+
+    it("Check data after set array of objects", function() {
+        var obj = new CoreObject();
+        obj._setDataEntity('entity', 'TestModule/EntityInherit', [{'_id' : 1}, {'_id' : 2}]);
+        obj._getDataKeyList().should.be.eql(['entity']);
+        obj._data.entity[0].should.be.instanceof(EntityInherit);
+        obj._data.entity[1].should.be.instanceof(EntityInherit);
+    });
+
+    it("Set data with an empty doc in array with entity non nullable", function() {
+        var obj = new CoreObject();
+        (function() {
+            obj._setDataEntity('entity', 'TestModule/EntityInherit', [], false);
+        }).should.throw("Null object non accepted");
+    });
+
+    it("Set data with an empty array with entity nullable", function() {
+        var obj = new CoreObject();
+        obj._setDataEntity('entity', 'TestModule/EntityInherit', [], true);
+        (obj._data.entity == null).should.be.true;
+    });
+
+    it("Set data with an empty doc in array with entity not nullable", function() {
+        var obj = new CoreObject();
+        (function() {
+            obj._setDataEntity('entity', 'TestModule/EntityInherit', [{'_id' : 1}, null, {'_id' : 2}], false);
+        }).should.throw("Null object non accepted in array");
+    });
+
+    it("Set data with an invalid identifier", function() {
+        var obj = new CoreObject();
+        (function() {
+            obj._setDataEntity('entity', 'TestModule/EntityInherit', [10]);
+        }).should.throw("Invalid identifier");
+    });
+
+    it("Set data with an invalid identifier", function() {
+        var obj = new CoreObject();
+        (function() {
+            obj._setDataEntity('entity', 'TestModule/EntityInherit', [{'test' : 10}]);
+        }).should.throw("Invalid identifier [Object in Array without _ID]");
+    });
 });
