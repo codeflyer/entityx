b2c6f58f3d1c075578d7610f50e58a751b39b525
Davide Fiorello <davide@codeflyer.com>
2014-11-16 20:30:01 +0100

    Added get repository to the entity parent class

--- tests/classesTest/lib/entities/EntityInherit.js
+++ tests/classesTest/lib/entities/EntityInherit.js
@@ -1,20 +1,11 @@
 var util = require('util');
 var Entity = require('./../../../../lib/entities/Entity');
-var Factory = require('./../../../../lib/Factory');
 
 var EntityInherit = function() {
-  Entity.call(this, {'useTimestamp': true});
+  Entity.call(this,
+      {'useTimestamp': true, repositoryName: 'TestModule/Inherit'});
 };
 util.inherits(EntityInherit, Entity);
-
-/**
- * @interfaceMethod
- * @returns {*}
- * @private
- */
-EntityInherit.prototype._getRepository = function() {
-  return Factory.getRepository('TestModule/Inherit', this.getId());
-};
 
 EntityInherit.prototype._loadDetails = function(details, callback) {
   this._setData('name', details.name);
